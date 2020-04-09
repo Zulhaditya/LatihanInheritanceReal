eb29b194a5e56e8c2653ca2598052a7d00255ca2
Zulhaditya <muhammad.hapiz@students.amikom.ac.id>
2020-04-09 12:03:59 +0700

    Add project files.

---
+++ LatihanInheritanceReal/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LatihanInheritanceReal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Adit", 19);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Thomas", 35, 190302, "Guru Matematika");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Ayana", 21, 10506, "Ayanajkt48@gmail.com");
+            student.GetNameAndAge();
+
+            Console.ReadKey();
+
+        }
+    }
+}
